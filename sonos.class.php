46e16903bf0e8ef9650e3d52be240a939a824249
Kasper Hartwich <kasper@hartwich.net>
2013-07-01 00:12:40 +0200

    Minor fix.

--- sonos.class.php
+++ sonos.class.php
@@ -85,7 +85,7 @@ class SonosController
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_URL, $this->url);
+        curl_setopt($ch, CURLOPT_URL, $this->url . $url);
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_TIMEOUT, 30);
